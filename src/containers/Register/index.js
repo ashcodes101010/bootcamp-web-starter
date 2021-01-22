8b352cb1c1fce23d9d9e5bbecd28faab7d3b4117
racemore <46336928+racemore@users.noreply.github.com>
2021-01-22 13:25:00 -0500

    Update index.js

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,19 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useMutation } from '@apollo/react-hooks'
 import { useHistory } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import {
   Container,
   LoginContainer,
-  Label,
   Input,
   Title,
   Button,
   Text,
-  IncPass
 } from './styles'
-import { REGISTER } from './graphql'
 
+import { REGISTER } from './graphql'
 
 const Register = () => {
   const history = useHistory()
@@ -21,124 +19,83 @@ const Register = () => {
   const [pass, setPass] = useState('')
   const [email, setEmail] = useState('')
   const [age, setAge] = useState('')
-  const [street, setStreet] = useState('')
-  const [city, setCity] = useState('')
-  const [state, setAState] = useState('')
-  const [country, setCountry] = useState('')
-  const [zip, setZip] = useState('')
-  const [register, {loading, error}] = useMutation(REGISTER, {
+  const [register] = useMutation(REGISTER, {
     variables: {
-      username,
-      email,
-      password: pass,
-      age,
-      address: {
-          street,
-          city,
-          state,
-          country
+      input: {
+        username,
+        email,
+        password: pass,
+        age: parseInt(age)
       }
     },
-    onCompleted: ({ register: { token, user: { id } } }) => {
+    onCompleted: ({ register: { token } }) => {
       localStorage.setItem('token', token)
-      localStorage.setItem('userId', id)
       history.push('/home')
       window.location.reload()
     },
+    onError: error => console.log(error)
 })
-
-const ValidEmail= () => {
-    const [emailCheck, setEmailCheck] = useState('')
-    useEffect(() => {
-      const fetchData = async () => {
-        const res = await fetch(`https://apilayer.net/api/check?access_key=d8f63b4dee87bd7953253e42a4b60767&email=${email}&format=1`)
-        const data = await res.json()
-        setEmailCheck(`${(data.emailCheck[0].format_valid) && (data.emailCheck[0].smtp_check)}`)
-      }
-      fetchData()
-    }, [])
-    return (
-      emailCheck
-    )
+const handleSubmit = async () => {
+  if (await fetchData()) {
+    register()
   }
+}
+
+const fetchData = async () => {
+  console.log(email)
+  const res = await fetch(`https://apilayer.net/api/check?access_key=d8f63b4dee87bd7953253e42a4b60767&email=${email}&format=1`)
+  const data = await res.json()
+  if(!data.email.format_valid && !data.email.smtp_check) {
+    console.log('Email does not exist anywhere')
+    return false
+  }
+
+}
 
   return (
     <Container>
       <LoginContainer>
         <Title>Register</Title>
-        <Label>Username</Label>
+        <Text>Kindly fill in your login details to proceed</Text>
         <Input
           type="text"
           name="username"
+          placeholder="Username"
           onChange={e => setUsername(e.target.value)}
           value={username}
         />
-        <Label>Email</Label>
         <Input
           type="text"
           name="email"
+          placeholder="Email"
           onChange={e => setEmail(e.target.value)}
           value={email}
         />
-        <Label>Password</Label>
         <Input
           type="password"
           name="password"
+          placeholder="Password"
           onChange={e => setPass(e.target.value)}
           value={pass}
         />
-        <Label>Age</Label>
         <Input
           type="number"
           name="age"
+          placeholder="Age (Must be 16+)"
           onChange={e => setAge(e.target.value)}
           value={age}
         />
-        <Label>Street Address</Label>
-        <Input
-          type="text"
-          name="street"
-          onChange={e => setStreet(e.target.value)}
-          value={street}
-        />
-        <Label>City</Label>
-        <Input
-          type="text"
-          name="city"
-          onChange={e => setCity(e.target.value)}
-          value={city}
-        />
-        <Label>State (optional)</Label>
-        <Input
-          type="text"
-          name="state"
-          onChange={e => setAState(e.target.value)}
-          value={state}
-        />
-        <Label>Country</Label>
-        <Input
-          type="text"
-          name="country"
-          onChange={e => setCountry(e.target.value)}
-          value={country}
-        />
-        <Label>Zip</Label>
-        <Input
-          type="number"
-          name="zip"
-          onChange={e => setZip(e.target.value)}
-          value={zip}
-        />
-        <Button onClick={register}>Register</Button>
+        <Button onClick={handleSubmit}>Register</Button>
+        <br></br>
         <Text>Already a Member?&nbsp;
             <Link to="/login">Log In</Link>
         </Text>
         <Text>
-          <ValidEmail />
         </Text>
       </LoginContainer>
     </Container>
   )
 }
+
 
 export default Register
